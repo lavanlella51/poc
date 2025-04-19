34c1b9091218066ab2fc240a0c7aa066b8ba5e1b
Lavan <78894842+lavanlella51@users.noreply.github.com>
2025-04-19 19:21:55 +0530

    With updated response

--- lwc_OTCHSContactDetails/lwc_OTCHSContactDetails.js
+++ lwc_OTCHSContactDetails/lwc_OTCHSContactDetails.js
@@ -19,4 +19,110 @@ export default class Lwc_OTCHSContactDetails extends LightningElement {
         })
     }
 
+    get apiMemberInfo(){
+        return {
+            "statusCode": "0000",
+            "statusDescription": "Success",
+            "data": {
+              "otchsMemberDashBoard": {
+                "OptedEmail": "optout",
+                "address": [
+                {
+                "changeReasonCode": null,
+                "city": "WOONSOCKET",
+                "country": null,
+                "county": null,
+                "firstName": null,
+                "lastName": null,
+                "line": [
+                "ONE CVS DRIVE",
+                "MC 1180"
+                ],
+                "postalCode": "02895",
+                "state": "RI",
+                "type": null,
+                "use": "Home"
+}
+                ],
+                "addressChangeIndicator": "Y",
+                "benefitDesc": null,
+                "benefits": [
+                {
+                "agentMessage": "One time courtesy claim can be processed for a missed benefit period, and another to reship an order that appears delivered but member claims non-receipt. Remember, these claims are to be processed by the MS team. Leave an open comment under MST-Member Outreach.",
+                "balanceAmount": 52,
+                "benefitAmount": 100,
+                "benefitDescription": "OTC MEDICARE",
+                "benefitPlan": "H0628-003-000",
+                "benefitUpdateDate": null,
+                "benefitUrl": null,
+                "canOrder": null,
+                "cardDisplayNumber": null,
+                "cardMemberId": "",
+                "catalogInd": null,
+                "catalogPdf": "250600.pdf",
+                "dcId": "OR",
+                "description": null,
+                "effectiveDate": null,
+                "expirationDate": null,
+                "faqHeader": null,
+                "faqPdf": "25_faq_aetna.pdf",
+                "flexCardOptinDate": "",
+                "flexCardOptinIndicator": "",
+                "householdInd": "test",
+                "id": "250600687",
+                "idType": "OTCHS_BENEFIT_ID_TYPE",
+                "invoiceLob": "Aetna National North Quarterly",
+                "logoFileName": "logo_aetna.png",
+                "maxOrders": "U",
+                "memberServicePhone": "1-833-570-6670",
+                "nextPeriodDate": null,
+                "orderCount": "0",
+                "ordersRemaining": null,
+                "otchsDedicatedPhone": "1-844-428-8147",
+                "otchsMemberStatus": {
+                    "disEnrollDate": "2025-01-11",
+                    "enrollDate": "2019-01-01",
+                    "status": "Enabled",
+                    "statusReason": "By valid date"
+                },
+                "typeIndicator": "O",
+                "subgroup": "Ohio/Kentucky",
+                "storeLimitInd": null,
+                "storeInd": "Y",
+                "status": null,
+                "startDate": null,
+                "segment": "000",
+                "rollOverIndicator": "No",
+                "rollOverAmt": 0,
+                "referenceId": null,
+                "purseName": "",
+                "programCode": "",
+                "premiumPackageCd": "0001",
+                "planTypeInd": null,
+                "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
+                "periodFactor": "Quarterly",
+                "paymentType": "OTC"
+                }
+                ],
+                "cellPhone": "5555551212",
+                "dateOfBirth": "2000-02-14",
+                "entityId": null,
+                "firstName": "AetnaNational10",
+                "lastName": "OTCHS",
+                "middleNameInitial": "X",
+                "otchsRegisteredEmailInfo": {
+                "email": "otcaetna10@cvs.com",
+                "pwdChangedDate": "",
+                "registeredDate": "2024-10-21",
+                "resentLinkDate": ""
+                },
+                "phoneNumber": "4017702500",
+                "planId": null,
+                "specialInstruction": "White Glove-UPS and Box",
+                "supportPhone": null
+                }
+            }
+        }
+    }
+
 }
--- lwc_OTCHSMemberDetails/lwc_OTCHSMemberDetails.js
+++ lwc_OTCHSMemberDetails/lwc_OTCHSMemberDetails.js
@@ -1,12 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Lwc_OTCHSMemberDetails extends LightningElement {
 
     benefitInformation = {};
     memberInformation = {};
 
-    connectedCallback(){
-        this.memberInformation = this.apiMemberInfo.result;
+    @api
+    get apiResponse() {
+        return this.memberInformation;
+    }
+
+    set apiResponse(value) {
+        this.transformResponse(value);    
+    }
+
+    transformResponse(value){
+        if(value && value.data && value.data.otchsMemberDashBoard){
+            let address = '';
+            for(let i=0; i < value.data.otchsMemberDashBoard.address.length; i++){
+                if(value.data.otchsMemberDashBoard.address[i].line && value.data.otchsMemberDashBoard.address[i].line.length > 0){
+                    address += value.data.otchsMemberDashBoard.address[i].line[0]
+                }
+                if(value.data.otchsMemberDashBoard.address[i].line && value.data.otchsMemberDashBoard.address[i].line.length > 1){
+                    address += ' ' +value.data.otchsMemberDashBoard.address[i].line[1]
+                }
+                if(value.data.otchsMemberDashBoard.address[i].city){
+                    address += ' ' + value.data.otchsMemberDashBoard.address[i].city;
+                }
+                if(value.data.otchsMemberDashBoard.address[i].state){
+                    address += ' ' + value.data.otchsMemberDashBoard.address[i].state;
+                }
+                if(value.data.otchsMemberDashBoard.address[i].postalCode){
+                    address += ' ' + value.data.otchsMemberDashBoard.address[i].postalCode;
+                }
+            }
+            let transformedResponse = {...value.data.otchsMemberDashBoard};
+            transformedResponse['memAddress'] = address;
+            transformedResponse['emailAddress'] = value.data.otchsMemberDashBoard.otchsRegisteredEmailInfo.email;
+            transformedResponse['optionalEmailAddress'] = value.data.otchsMemberDashBoard.otchsRegisteredEmailInfo.optionalEmail;
+            
+            this.memberInformation = transformedResponse;
+
+        }else{
+            this.memberInformation = [];
+        }
     }
     
     toggleMemberSection(){
@@ -31,6 +68,16 @@ export default class Lwc_OTCHSMemberDetails extends LightningElement {
         this.template.querySelector('.status-popover').classList.remove("slds-show");
     }
 
+    showBenefitPopover(){
+        this.template.querySelector('.benefit-popover').classList.remove("slds-hide");
+        this.template.querySelector('.benefit-popover').classList.add("slds-show");
+    }
+
+    hideBenefitPopover(){
+        this.template.querySelector('.benefit-popover').classList.add("slds-hide");
+        this.template.querySelector('.benefit-popover').classList.remove("slds-show");
+    }
+
     showHouseHoldTable(){
         this.template.querySelector('.household-table-section').classList.toggle("slds-hide");
     }
@@ -40,315 +87,18 @@ export default class Lwc_OTCHSMemberDetails extends LightningElement {
         console.log('-------'+event.target.label);
     }
 
-    get apiMemberInfo(){
-        return {
-          "success": true,
-          "statusCode": 200,
-          "message": "Success",
-          "result": {
-                "memberEntityId": "0",
-                "alternativeId": "",
-                "memberId": "otcaetna10",
-                "firstName": "AetnaNational10",
-                "lastName": "OTCHS",
-                "middleNameInitial": "X",
-                "dob": "2000-02-14",
-                "address_1": "ONE CVS DRIVE",
-                "address_2": "MC 1180",
-                "city": "WOONSOCKET",
-                "stateCode": "RI",
-                "zipcode": "02895",
-                "status": null,
-                "statusReason": null,
-                "phoneNumber": "4017702500",
-                "cellPhone": "5555551212",
-                "year": "2025",
-                "email": "otcaetna10@cvs.com",
-                "optionalEmail": "optout",
-                "verified": "Yes",
-                "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
-                "url": null,
-                "lockedDt": "",
-                "fraudDt": "",
-                "planActInd": "Y",
-                "opAddrChngInd": "Y",
-                "specialHandlingDsc": "White Glove-UPS and Box",
-                "paymentUrl": "https://sycuriolab.cvshealth.com/semafone/service/capture/getSycurioIncommTokenFragment.html",
-                "ordHistList": [
-                
-            ],
-                "claimHistList": [
-                
-            ],
-                "commentHistList": [
-                
-            ],
-                "oneCardInd": "",
-                "ncpdpInd": "N",
-                "shipMethodInd": "B",
-                "deliverRegularDate": "2025-04-15",
-                "deliverHazmatDate": "2025-04-15",
-                "deliverWhitegloveDate": "2025-04-12",
-                "dateRegistered": "2024-10-21",
-                "datePasswordChanged": "",
-                "dateResentLink": "",
-                "paymentType": "OTC",
-                "sponsorId": "",
-                "sponsorName": "",
-                "hdPobInd": "N",
-                "benefits": [
-                  {
-                        "benefitId": "250600687",
-                        "benefitAmount": 100.0,
-                        "benefitPlan": "H0628-003-000",
-                        "benefitTypeInd": "O",
-                        "logoFilename": "logo_aetna.png",
-                        "canOrder": false,
-                        "orderCount": 0,
-                        "catalogId": 0,
-                        "catalogPdf": "250600.pdf",
-                        "invoiceLob": "Aetna National North Quarterly",
-                        "status": "Enabled",
-                        "statusReason": "By valid date",
-                        "periodFactor": "Quarterly",
-                        "maxOrders": "U",
-                        "rolloverInd": "No",
-                        "rolloverAmt": 0.0,
-                        "faqPdf": "25_faq_aetna.pdf",
-                        "storeInd": "Y",
-                        "storeLimitInd": "Y",
-                        "memberServicePhone": "1-833-570-6670",
-                        "ttyPhone": null,
-                        "otchsDedicatedPhone": "1-844-428-8147",
-                        "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
-                        "ncpdpInd": "N",
-                        "premiumPackageCd": "0001",
-                        "shipMethodInd": "B",
-                        "nextPeriodDate": null,
-                        "dcId": "OR",
-                        "sponsorId": "",
-                        "sponsorName": "",
-                        "benefitDescription": "OTC MEDICARE 1",
-                        "enrollDate": "2019-01-01",
-                        "disenrollDate": "2025-01-11",
-                        "catalogDescription": "Aetna Medicare",
-                        "subgroup": "Ohio/Kentucky",
-                        "segment": "000",
-                        "totalOrders": "3",
-                        "url": "https://www.cvs.com/benefits",
-                        "paymentType": "OTC",
-                        "memberReferenceId": null,
-                        "cardSerialNumber": null,
-                        "hdPobInd": "N",
-                        "planTypeInd": "O",
-                        "programCd": "",
-                        "adminAlias": "",
-                        "year": 2025,
-                        "purseOptInInd": "",
-                        "flexcardOptInDatetime": "",
-                        "lastBenefitUpdate": "2025-01-10 12:14:02",
-                        "benefitStartDt": "2025-01-01",
-                        "purseName": "",
-                        "cardMemberId": "",
-                        "catalogInd": "Y",
-                        "household": "",
-                        "householdInd": "No",
-                        "agentMessage": "One time courtesy claim can be processed for a missed benefit period, and another to reship an order that appears delivered but member claims non-receipt. Remember, these claims are to be processed by the MS team. Leave an open comment under MST-Member Outreach.",
-                        "balanceAmt": 52.0      
-            },
-            {
-                        "benefitId": "250600688",
-                        "benefitAmount": 100.0,
-                        "benefitPlan": "H0628-003-000",
-                        "benefitTypeInd": "O",
-                        "logoFilename": "logo_aetna.png",
-                        "canOrder": false,
-                        "orderCount": 0,
-                        "catalogId": 0,
-                        "catalogPdf": "250600.pdf",
-                        "invoiceLob": "Aetna National North Quarterly",
-                        "status": "Enabled",
-                        "statusReason": "By valid date",
-                        "periodFactor": "Quarterly",
-                        "maxOrders": "U",
-                        "rolloverInd": "No",
-                        "rolloverAmt": 0.0,
-                        "faqPdf": "25_faq_aetna.pdf",
-                        "storeInd": "Y",
-                        "storeLimitInd": "Y",
-                        "memberServicePhone": "1-833-570-6670",
-                        "ttyPhone": null,
-                        "otchsDedicatedPhone": "1-844-428-8147",
-                        "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
-                        "ncpdpInd": "N",
-                        "premiumPackageCd": "0001",
-                        "shipMethodInd": "B",
-                        "nextPeriodDate": null,
-                        "dcId": "OR",
-                        "sponsorId": "",
-                        "sponsorName": "",
-                        "benefitDescription": "OTC MEDICARE 2",
-                        "enrollDate": "2019-01-01",
-                        "disenrollDate": "2025-01-11",
-                        "catalogDescription": "Aetna Medicare",
-                        "subgroup": "Ohio/Kentucky",
-                        "segment": "000",
-                        "totalOrders": "3",
-                        "url": "https://www.cvs.com/benefits",
-                        "paymentType": "OTC",
-                        "memberReferenceId": null,
-                        "cardSerialNumber": null,
-                        "hdPobInd": "N",
-                        "planTypeInd": "O",
-                        "programCd": "",
-                        "adminAlias": "",
-                        "year": 2025,
-                        "purseOptInInd": "",
-                        "flexcardOptInDatetime": "",
-                        "lastBenefitUpdate": "2025-01-10 12:14:02",
-                        "benefitStartDt": "2025-01-01",
-                        "purseName": "",
-                        "cardMemberId": "",
-                        "catalogInd": "Y",
-                        "household": "",
-                        "householdInd": "No",
-                        "agentMessage": "One time courtesy claim can be processed for a missed benefit period, and another to reship an order that appears delivered but member claims non-receipt. Remember, these claims are to be processed by the MS team. Leave an open comment under MST-Member Outreach.",
-                        "balanceAmt": 52.0      
-            }    ,
-            {
-                        "benefitId": "250600689",
-                        "benefitAmount": 100.0,
-                        "benefitPlan": "H0628-003-000",
-                        "benefitTypeInd": "O",
-                        "logoFilename": "logo_aetna.png",
-                        "canOrder": false,
-                        "orderCount": 0,
-                        "catalogId": 0,
-                        "catalogPdf": "250600.pdf",
-                        "invoiceLob": "Aetna National North Quarterly",
-                        "status": "Enabled",
-                        "statusReason": "By valid date",
-                        "periodFactor": "Quarterly",
-                        "maxOrders": "U",
-                        "rolloverInd": "No",
-                        "rolloverAmt": 0.0,
-                        "faqPdf": "25_faq_aetna.pdf",
-                        "storeInd": "Y",
-                        "storeLimitInd": "Y",
-                        "memberServicePhone": "1-833-570-6670",
-                        "ttyPhone": null,
-                        "otchsDedicatedPhone": "1-844-428-8147",
-                        "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
-                        "ncpdpInd": "N",
-                        "premiumPackageCd": "0001",
-                        "shipMethodInd": "B",
-                        "nextPeriodDate": null,
-                        "dcId": "OR",
-                        "sponsorId": "",
-                        "sponsorName": "",
-                        "benefitDescription": "OTC MEDICARE 3",
-                        "enrollDate": "2019-01-01",
-                        "disenrollDate": "2025-01-11",
-                        "catalogDescription": "Aetna Medicare",
-                        "subgroup": "Ohio/Kentucky",
-                        "segment": "000",
-                        "totalOrders": "3",
-                        "url": "https://www.cvs.com/benefits",
-                        "paymentType": "OTC",
-                        "memberReferenceId": null,
-                        "cardSerialNumber": null,
-                        "hdPobInd": "N",
-                        "planTypeInd": "O",
-                        "programCd": "",
-                        "adminAlias": "",
-                        "year": 2025,
-                        "purseOptInInd": "",
-                        "flexcardOptInDatetime": "",
-                        "lastBenefitUpdate": "2025-01-10 12:14:02",
-                        "benefitStartDt": "2025-01-01",
-                        "purseName": "",
-                        "cardMemberId": "",
-                        "catalogInd": "Y",
-                        "household": "",
-                        "householdInd": "No",
-                        "agentMessage": "One time courtesy claim can be processed for a missed benefit period, and another to reship an order that appears delivered but member claims non-receipt. Remember, these claims are to be processed by the MS team. Leave an open comment under MST-Member Outreach.",
-                        "balanceAmt": 52.0      
-            }    ,
-            {
-                        "benefitId": "250600680",
-                        "benefitAmount": 100.0,
-                        "benefitPlan": "H0628-003-000",
-                        "benefitTypeInd": "O",
-                        "logoFilename": "logo_aetna.png",
-                        "canOrder": false,
-                        "orderCount": 0,
-                        "catalogId": 0,
-                        "catalogPdf": "250600.pdf",
-                        "invoiceLob": "Aetna National North Quarterly",
-                        "status": "Enabled",
-                        "statusReason": "By valid date",
-                        "periodFactor": "Quarterly",
-                        "maxOrders": "U",
-                        "rolloverInd": "No",
-                        "rolloverAmt": 0.0,
-                        "faqPdf": "25_faq_aetna.pdf",
-                        "storeInd": "Y",
-                        "storeLimitInd": "Y",
-                        "memberServicePhone": "1-833-570-6670",
-                        "ttyPhone": null,
-                        "otchsDedicatedPhone": "1-844-428-8147",
-                        "planMessage": "2025 Catalogs will be mailed to all Aetna Medicare members starting mid December - January 15; Review comment history before requesting a new catalog after January 15th.",
-                        "ncpdpInd": "N",
-                        "premiumPackageCd": "0001",
-                        "shipMethodInd": "B",
-                        "nextPeriodDate": null,
-                        "dcId": "OR",
-                        "sponsorId": "",
-                        "sponsorName": "",
-                        "benefitDescription": "OTC MEDICARE 4",
-                        "enrollDate": "2019-01-01",
-                        "disenrollDate": "2025-01-11",
-                        "catalogDescription": "Aetna Medicare",
-                        "subgroup": "Ohio/Kentucky",
-                        "segment": "000",
-                        "totalOrders": "3",
-                        "url": "https://www.cvs.com/benefits",
-                        "paymentType": "OTC",
-                        "memberReferenceId": null,
-                        "cardSerialNumber": null,
-                        "hdPobInd": "N",
-                        "planTypeInd": "O",
-                        "programCd": "",
-                        "adminAlias": "",
-                        "year": 2025,
-                        "purseOptInInd": "",
-                        "flexcardOptInDatetime": "",
-                        "lastBenefitUpdate": "2025-01-10 12:14:02",
-                        "benefitStartDt": "2025-01-01",
-                        "purseName": "",
-                        "cardMemberId": "",
-                        "catalogInd": "Y",
-                        "household": "",
-                        "householdInd": "No",
-                        "agentMessage": "One time courtesy claim can be processed for a missed benefit period, and another to reship an order that appears delivered but member claims non-receipt. Remember, these claims are to be processed by the MS team. Leave an open comment under MST-Member Outreach.",
-                        "balanceAmt": 52.0      
-            }        
-            ]  
-        }
-        };
-    }
-
     get benefitOptions(){
         let benefitOptionArray = [];
-        let memberInfo = this.apiMemberInfo.result.benefits;
-        for (var key in memberInfo) {
-            console.log(memberInfo[key].benefitId);
+        let benefits = this.memberInformation.benefits;
+
+        for (var key in benefits) {
+            console.log(benefits[key].benefitId);
             if(key === '0'){
-                this.benefitInformation =  memberInfo[key];   
+                this.benefitInformation =  benefits[key];   
             }
             benefitOptionArray.push({
-                "label" : memberInfo[key].benefitDescription,
-                "value" : memberInfo[key].benefitId,
+                "label" : benefits[key].benefitDescription,
+                "value" : benefits[key].benefitId,
                 "checked" : key === '0'
             });
         }
