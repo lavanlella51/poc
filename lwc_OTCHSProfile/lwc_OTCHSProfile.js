cb6ae878c9fd2466fcc27001703803510585c04b
Lavan <78894842+lavanlella51@users.noreply.github.com>
2025-04-29 01:47:04 +0530

    With cart items

--- lwc_OTCHSProfile/lwc_OTCHSProfile.js
+++ lwc_OTCHSProfile/lwc_OTCHSProfile.js
@@ -1,57 +1,140 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track  } from 'lwc';
+//import {makeCallOut}  from 'c/ssCompLwcApexContinuation';
+//import getCachedData from '@salesforce/apex/OtchsMemberInfoCache.getCachedmembershipJson';
 
-export default class Lwc_OTCHSProfile extends LightningElement {
+
+export default class OTCHSOrderCreationLwc extends LightningElement {
 
     cartItems = [];
     catalogItems = [];
     serverData = [];
 
     connectedCallback(){
-        let items = [];
+        /*let items = [];
         for(let i=1; i < 5; i++){
             items.push({"label":"Placeholder Cart Item "+i, "amount" : 10*i, "id":i});
         }
-        this.cartItems = items;
+        this.cartItems = items;*/
+        this.getCatalogInfo();
 
-        let records = this.catalogInfo.response.data.getOTCHSPortalCatalog;
-        let recordMap = new Map();
-        for(let i=0; i < records.length; i++){
-            let key = records[i].departmentDscription;
-            console.log('----key-----'+key);
-            console.log('----has-----'+recordMap.has(key));
-            let transformedObj = {...records[i]};
-            transformedObj['serialNumber'] = i+1;
-            transformedObj['isOutOfStock'] = records[i].availableBohQty === "0" ? true : false;
-            transformedObj['disaleInput'] = records[i].recallInd == "Y" || records[i].availableBohQty === "0" ? true : false;
-            transformedObj['maxQuantity'] = this.minValue(parseInt(records[i].availableBohQty), parseInt(records[i].limits));
-            if (!recordMap.has(key)) {
-                console.log('----inside false-----');
-                recordMap.set(key, [transformedObj]);
-            } else{
-                console.log('----inside true-----');
-                recordMap.get(key).push(transformedObj);
+
+        /*getCachedData({membershipId: '123'})
+        .then(result => {
+            console.log('---tile result---'+JSON.stringify(result))
+            
+          
+        })
+        .catch(error => {
+            console.log('Comments --> error: '+error);
+            console.log('Comments --> error: '+JSON.stringify(error.body.message));
+        }); */
+    }
+
+    getCatalogInfo(){
+        
+            let reqBody = {
+                "data": {
+                    "id": "1",
+                    "idType": "OTCHS_MEMBER_ID_TYPE",
+                    "otchsMemberInfoInput": {
+                    "benefitID": "251300028",
+                    "caseStatus": "Y",
+                    "claimOrderTypeEnum": "OTCHS_ORDER_ID",
+                    "enrolled": true,
+                    "entityID": "",
+                    "formId": "251320",
+                    "memberID": "otcaetna16",
+                    "orderID": "14451733",
+                    "paymentType": "TRUEINCOMM",
+                    "planID": "13",
+                    "planTypeIndicator": "",
+                    "programCode": "PCA8-4905",
+                    "zipCode": null
+            }
+                }
+            };
+            this.runApi(reqBody);
+    }
+    receivedData;
+    runApi(requestBody){
+      
+    
+            let parsedResponse = this.catalogInfo();
+            console.log('----parsedResponse----'+JSON.stringify(parsedResponse));
+            let records = parsedResponse.response.data.getOTCHSPortalCatalog;
+            let recordMap = new Map();
+            for(let i=0; i < records.length; i++){
+                let key = records[i].departmentDscription;
+                console.log('----key-----'+key);
+                console.log('----has-----'+recordMap.has(key));
+                let transformedObj = {...records[i]};
+                transformedObj['serialNumber'] = i+1;
+                transformedObj['isOutOfStock'] = records[i].availableBohQty === "0" ? true : false;
+                transformedObj['disaleInput'] = records[i].recallInd == "Y" || records[i].availableBohQty === "0" ? false : false;
+                transformedObj['maxQuantity'] = this.minValue(parseInt(records[i].availableBohQty), parseInt(records[i].limits));
+                if (!recordMap.has(key)) {
+                    console.log('----inside false-----');
+                    recordMap.set(key, [transformedObj]);
+                } else{
+                    console.log('----inside true-----');
+                    recordMap.get(key).push(transformedObj);
+                }
+            }
+            let catalogs = [];
+            for (let [key, value] of recordMap) {
+                console.log(key + " is " + value);
+                catalogs.push({"description":key, "tableData": value, "identifier":key.replaceAll(' ','')});
+            }
+            this.catalogItems = catalogs;
+            this.serverData = catalogs;
+        
+    }
+
+    handleAddCart(event){
+        console.log('---input num---'+event.target.value);
+        console.log('----'+event.target.dataset.skuid);
+        console.log('----'+event.target.dataset.description);
+        let tableRecords = this.catalogItems.find(item => item.identifier === event.target.dataset.description).tableData;
+        console.log('-----sp-----'+JSON.stringify(tableRecords));
+        let selectedProduct = tableRecords.find(item => item.skuId === event.target.dataset.skuid);
+        let items = [...this.cartItems];
+
+        let isExisting = false;
+        for(let i=0; i < items.length; i++){
+            if(items[i].id === selectedProduct.skuId){  
+                isExisting = true;              
+                items[i].quantity = event.target.value;
+                items[i].total = (selectedProduct.price * event.target.value);
+                break;    
             }
         }
-        let catalogs = [];
-        for (let [key, value] of recordMap) {
-            console.log(key + " is " + value);
-            catalogs.push({"description":key, "tableData": value});
+        if(!isExisting){
+            items.push({"label": selectedProduct.description, "amount" : selectedProduct.price, "total" : (selectedProduct.price * event.target.value), "id":selectedProduct.skuId, "quantity":event.target.value, "imageUrl": selectedProduct.imageUrl});
         }
-        this.catalogItems = catalogs;
-        this.serverData = catalogs;
+        console.log('-----items-----'+JSON.stringify(items));
+        this.cartItems = [...items];
+    }
+
+    handleCartDelete(event){
+        console.log('---delete id---'+event.target.dataset.id);
+        let items = [...this.cartItems];
+        let index = items.findIndex(item => item.id === event.target.dataset.id);
+        items.splice(index, 1);
+        this.cartItems = [...items];
     }
 
     minValue(val1, val2){
         return Math.min(val1, val2);
     }
 
-    handleSearch(event){
+        handleSearch(event){
         console.log('---search value---'+event.target.value);
         if(event.target.value != '' && event.target.value != null){
             this.filterResults(event.target.value);
         }else{
             this.catalogItems = [...this.serverData];
         }
+        
     }
 
     filterResults(filterText){
@@ -60,14 +143,208 @@ export default class Lwc_OTCHSProfile extends LightningElement {
             let filteredResults = [];
             filteredResults = this.serverData[i].tableData.filter(record => JSON.stringify(record).toLowerCase().includes(filterText.toLowerCase()));       
             if(filteredResults.length > 0){
-                catalogs.push({"description": this.serverData[i].description, "tableData": filteredResults});
+                catalogs.push({"description": this.serverData[i].description, "tableData": filteredResults, "identifier":this.serverData[i].description.replaceAll(' ','')});
             }
         }
         console.log('----catalogs-----'+JSON.stringify(catalogs));
         this.catalogItems = [...catalogs];
     }
 
-    get catalogInfo(){
+    toggleSection(){
+        
+    }
+
+    showModal
+    handleAddressChange(){
+
+        this.showModal = true;
+    }
+
+    validationMsg ='';
+    showValidationMsg = false;
+    isChangeAddress = true;
+    @track addressValidationResult = {};
+    @track editAddressObj = {};
+    disableInfoBtn  = true;
+
+    sendAddressValidationRequest(event){
+      
+        let requestBody = event.detail;
+
+        makeCallOut('OTCHS_Address_APIGEE', JSON.stringify(requestBody)).then(result => {
+            console.log('-----result-----'+JSON.stringify(result)) ;
+
+            let value = JSON.parse(result.response);
+            console.log('-----result-----'+value) ;
+
+            if(value.statusCode == 'ERROR'){
+                this.template.querySelector('c-bell-credit-card-toast-lwc').showToast('error', '<strong>System is not Responding</strong>',
+                JSON.stringify(value.description), 'utility:warning', 10000);
+            }
+            else{
+            this.addressValidationResult = value;
+            }
+            
+        })
+        .catch(error => {
+            console.log('Comments --> error: ' + error);
+            console.log('Comments --> error: ' + JSON.stringify(error.body.message));
+        });
+
+    }
+
+   
+
+  
+    /* API response, reading the JSON and setting the values to properties */
+    set jsonaddressValidationResponse(value) {
+     //   console.log('----address validation response-----'+JSON.stringify(value));
+        if(value.statusCode == 'ERROR'){
+            this.template.querySelector('c-bell-credit-card-toast-lwc').showToast('error', '<strong>System is not Responding</strong>',
+            JSON.stringify(value.description), 'utility:warning', 10000);
+        }
+        else{
+        this.addressValidationResult = value;
+        }
+    }
+    @track validatedAdressResponse = {};    
+    handleSelectedAddress(event){
+        console.log('-------handle selected address-------'+JSON.stringify(event.detail.address1));
+       
+        this.validatedAdressResponse = event.detail;
+        this.disableInfoBtn = false;        
+    }
+
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.showModal = false;
+       
+    }
+
+    handleCancelButtonKeydown(event){
+        if(event.which == 9){
+            event.preventDefault();
+            if(event.shiftKey){
+                if(this.template.querySelector(".child-panel")){
+                    this.template.querySelector(".child-panel").focus();   
+                }else if(this.template.querySelector(".last-input")){
+                    this.template.querySelector(".last-input").focus();
+                }
+            }else{
+                if(this.disableInfoBtn){
+                    this.template.querySelector(".modalClose").focus();
+                }else{
+                    this.template.querySelector(".submitBtn").focus();
+                }
+            }
+        }
+    }
+
+    handleCloseButtonKeydown(event){
+        if(event.which == 9){
+            event.preventDefault();
+            if(event.shiftKey){
+                if(this.disableInfoBtn){
+                    this.template.querySelector(".closeBtn").focus();
+                }else{
+                    this.template.querySelector(".submitBtn").focus();
+                }   
+            }else{
+                if(this.template.querySelector("lightning-input")){
+                    this.template.querySelector("lightning-input").focus();
+                }else if(this.template.querySelector(".child-panel")){
+                    this.template.querySelector(".child-panel").focus();
+                }
+            }
+        }
+    }
+
+    handleSubmitButtonKeydown(event){
+        if(event.which == 9){
+            event.preventDefault();
+            if(event.shiftKey){
+                this.template.querySelector(".closeBtn").focus();   
+            }else{
+                this.template.querySelector(".modalClose").focus();
+            }
+        }
+    }
+
+    handleSubmitOrder(){
+        console.log('------handle submit order-----');
+        
+        let requestBody = {
+                "data": {
+                  "idType": "OTCHS_AGENT_ID_TYPE",
+                  "addItemsToBasketInput": {
+                    "benefitId": "m2hTKggHBZHQrjnRZOeodQ==",
+                    "memberId": "94",
+                    "planId": "17",
+                    "items": [
+                      {
+                        "dcId": "AU",
+                        "benefitUPC": "P38",
+                        "idType": "OTCHS",
+                        "id": "255045",
+                        "qty": "1",
+                        "shippingMethod": "500",
+                        "stockLevel": "30"
+                      }
+                    ],
+                    "orderType": "RETAIL_OTCHS_ORDER_ID_TYPE"
+                  },
+                  "id": "94",
+                  "planId": "17"
+              }
+
+        };
+       
+
+        makeCallOut('OtchsAddBasket', JSON.stringify(requestBody)).then(result => {
+            console.log('-----result-----'+JSON.stringify(result)) ;
+            let value = JSON.parse(result.response);
+            console.log('-----result-----'+value) ;
+            this.makegetOrderdetailscall(result);
+            
+        })
+        .catch(error => {
+            console.log('Comments --> error: ' + error);
+            console.log('Comments --> error: ' + JSON.stringify(error.body.message));
+        });
+    }
+
+    makegetOrderdetailscall(result){
+        console.log('-----makegetOrderdetailscall-----'+result);
+        let responseofAddBasket = JSON.parse(result.response);  
+        let requestBody = {
+            "data": {
+                "id": "",
+                "idType": "OTCHS_AGENT_ID_TYPE",
+                "planId": "",
+                "orderFilterInput": {
+                  "memberId": "94",
+                  "planId": "17",
+                  "pageName": "cart",
+                  "orderLite": false,
+                  "idType": "RETAIL_OTCHS_ORDER_ID_TYPE",
+                  "benefitId": "m2hTKggHBZHQrjnRZOeodQ=="
+                }
+              }
+
+        };
+        makeCallOut('OtchsGetOrderDetails', JSON.stringify(requestBody)).then(result => {
+            console.log('-----result-----'+JSON.stringify(result)) ;
+            let value = JSON.parse(result.response);
+            console.log('-----result-----'+value) ;
+
+        })
+        .catch(error => {
+            console.log('Comments --> error: ' + error);    
+            console.log('Comments --> error: ' + JSON.stringify(error.body.message));
+        });
+    }
+
+    catalogInfo(){
         return {
             "response": {
             "statusCode": "0000",
@@ -239,4 +516,5 @@ export default class Lwc_OTCHSProfile extends LightningElement {
         }
         }
     }
+
 }
